d55ea944d16e1c90f521769a12042f10071fa3e4
Alex Wallace <alexwallax@gmail.com>
2022-01-14 10:57:51 -0300

    finalizando o projeto front-end gameflix

--- js/script.js
+++ js/script.js
@@ -4,12 +4,10 @@ document.getElementById("link").onclick = function() {
     /*location.href = "https://www.youtube.com/watch?v=YDhk1Ef8W9o";*/ //aqui abre na mesma janela
 
     window.open('https://www.youtube.com/watch?v=YDhk1Ef8W9o'); // aqui abre em outra janela
-
 }
 
 /*direciona click no botão mais informações para uma nova janela info.html*/ 
 document.getElementById("info").onclick = function() {
 
     window.open('info.html'); 
-
 }
